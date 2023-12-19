4fd968b89060440ed78477df0769eb59d4f4b929
Wasif Haque <wasif.haque@education.gov.uk>
2023-12-19 16:03:05 +0000

    File drop animation

--- UI/f1-tyre-deg-UI/src/app/home/home.component.ts
+++ UI/f1-tyre-deg-UI/src/app/home/home.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
-  onFileUpload() {
+export class HomeComponent {
+  defaultCss: string = "drop-area";
+  draggedOverCss: string = "drag-over-drop-area";
+  dropAreaCss: string = this.defaultCss;
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dropAreaCss = this.draggedOverCss;
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dropAreaCss = this.defaultCss;
+  }
+
+  onFileUpload(files: any) {
 
   }
 }
